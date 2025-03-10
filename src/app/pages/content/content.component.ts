1025b2456da274957b4b8c21f12bee7000237862
Geo <gjsj@discente.ifpe.edu.br>
2025-03-10 14:51:09 -0300

    components dinamycs propertys

---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent implements OnInit {
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+
+  constructor(){}
+  
+  ngOnInit(): void {
+      
+  }
+}
