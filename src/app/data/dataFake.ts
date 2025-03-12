3a536bf3d12d926e3d4b075f0e62a9da1312d082
Geo <gjsj@discente.ifpe.edu.br>
2025-03-12 02:52:43 -0300

    adding formation data

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,21 @@
 export const dataFake = [
     {
         "id":"1",
-        "title": "Novidades no multiverso",
-        "description": "A chegada de um novo homem aranha muda o jogo!",
-        "photoCover":"https://ibitelecom.com.br/wp-content/uploads/2023/11/image-8.jpeg"
+        "title": "Formação Java Developer",
+        "description": "Ser um Java Developer exige domínio de Java, Spring Boot, bancos de dados, APIs REST, testes e arquitetura de software. Também envolve POO, padrões de projeto e boas práticas. Com essa formação, é possível atuar em web, mobile, aplicações corporativas e microsserviços, sendo uma carreira valorizada no mercado.",
+        "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQEyKrNmTMS7Yg/feedshare-shrink_1280/B4DZWBo8ERG8Ak-/0/1741636748431?e=1744243200&v=beta&t=zYy07_NhT1xt4UgCFlvt4Te3M7xoaM9GKCnnDrQAxk4"
     },
     {
         "id":"2",
-        "title": "Mudanças no balanceamento do jogo!",
-        "description": "",
-        "photoCover":"https://i0.wp.com/ovicio.com.br/wp-content/uploads/2024/09/20240903-multiverso-marvel.jpg?fit=1192%2C670&ssl=1"
+        "title": "GFT - Desenvolvimento Java com IA",
+        "description": "O curso abordou desde os fundamentos de Java até tópicos avançados, como modelagem de bancos de dados e desenvolvimento de APIs com Spring Boot na nuvem. Além disso, tive a oportunidade de trabalhar em 5 projetos práticos, que me ajudaram a aprimorar minhas habilidades e preparar meu portfólio para o mercado de trabalho. Estou animado para aplicar o que aprendi em novos desafios e contribuir para soluções robustas e escaláveis!",
+        "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQEB66maFRrJ3A/feedshare-shrink_1280/B4DZWBo8DXGkAk-/0/1741636748119?e=1744848000&v=beta&t=nHoLjg4nOHPQ8oeVzbuJojuW9AXByYUDeTCZWvKls7g"
+    },
+    {
+        "id":"3",
+        "title": "Formação React Developer",
+        "description": "Nesta formação, aprendi a criar Web Apps dinâmicas utilizando Single Page Application (SPA), entendendo suas vantagens e aplicabilidades. Explorei o poder das páginas componentizadas, aprendi a trabalhar com gerenciamento de estados e a implementar roteamento, tudo isso enquanto compunha layouts ricos e bem estruturados, utilizando a principal biblioteca do Facebook",
+        "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQHhREg258daDw/feedshare-shrink_2048_1536/B4DZWBo8EiGkAs-/0/1741636748634?e=1744848000&v=beta&t=UcCizmqbwmid0Jqv0N1NDmrUimeeOrYmNz4R2ObsBLM"
     }
 
 ]
