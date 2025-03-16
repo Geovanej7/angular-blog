8f11d95611a3837a8a87006c498a476b77573681
Geo <gjsj@discente.ifpe.edu.br>
2025-03-15 23:43:19 -0300

    update certificates

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -16,6 +16,19 @@ export const dataFake = [
         "title": "Formação React Developer",
         "description": "Nesta formação, aprendi a criar Web Apps dinâmicas utilizando Single Page Application (SPA), entendendo suas vantagens e aplicabilidades. Explorei o poder das páginas componentizadas, aprendi a trabalhar com gerenciamento de estados e a implementar roteamento, tudo isso enquanto compunha layouts ricos e bem estruturados, utilizando a principal biblioteca do Facebook",
         "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQHhREg258daDw/feedshare-shrink_2048_1536/B4DZWBo8EiGkAs-/0/1741636748634?e=1744848000&v=beta&t=UcCizmqbwmid0Jqv0N1NDmrUimeeOrYmNz4R2ObsBLM"
+    }, 
+    {
+        "id":"4",
+        "title": "Decola tech",
+        "description": "Durante o bootcamp Decola Tech 2025, aprimorei meus conhecimentos em Java e Spring, desenvolvendo aplicações robustas. Aprendi a criar e otimizar consultas SQL, construí e consumi APIs RESTful, aplicando boas práticas de integração. Também explorei Maven e Gradle para gerenciar dependências e automatizar builds. Tive contato com Inteligência Artificial (IA) e iniciei no desenvolvimento front-end com Angular. ",
+        "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQGOZZs3Dqwsqw/feedshare-shrink_1280/B4DZWcyE4pH4As-/0/1742092128607?e=1744848000&v=beta&t=tPB6Gf97fdkJdYeCLAR1CidMqs1_ud-OGQRX7Xe8sSQ"
+    },
+    {
+        "id":"5",
+        "title": "Basic Front-end",
+        "description": "Foram 240 horas de aprendizado intenso, onde aprimorei meus conhecimentos em desenvolvimento front-end, fortalecendo minha base para construir interfaces modernas e responsivas.",
+        "photoCover":"https://media.licdn.com/dms/image/v2/D4D22AQECKZKEDIA1Hw/feedshare-shrink_2048_1536/B4DZWcyE4lG4As-/0/1742092128643?e=1744848000&v=beta&t=-T4Qs1b1lZfjbNf1UWKX7FSGI2Cd0bToXW9_3hB5G-I"
     }
+
 
 ]
